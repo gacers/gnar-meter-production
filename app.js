d706ed41bbb81d635d7b1e57ab1f8456f9b75c11
Gary Acers <gacers@hugeinc.com>
2018-01-24 15:22:01 -0500

    fix v7.0

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'hbs');
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://cams.cdn-surfline.com/*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
