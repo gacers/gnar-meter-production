091e6b177847cf1559c81b75d04b442583e55ba3
Gary Acers <gacers@hugeinc.com>
2018-01-24 15:15:20 -0500

    fix v5.0

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 
 var app = express();
+
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://cams.cdn-surfline.com/*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,11 +29,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
