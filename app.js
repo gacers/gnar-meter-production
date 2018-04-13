f05642f0cc9107057a27219c8062a1c0f4f16eef
Gary Acers <gacers@hugeinc.com>
2018-04-13 10:31:12 -0400

    New crop try

--- app.js
+++ app.js
@@ -14,7 +14,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(cors({ origin: " https://cams.cdn-surfline.com" }));
+app.use(cors({ credentials: true, origin: 'https://cams.cdn-surfline.com/*' }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
