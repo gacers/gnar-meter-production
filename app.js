c422ae59c7f4c50299af72cd3dc5936fb6fd176c
Gary Acers <gacers@hugeinc.com>
2018-04-13 10:23:52 -0400

    Adding cors

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require("cors");
+
 
 var index = require('./routes/index');
 
 var app = express();
+
+app.use(cors({ origin: " https://cams.cdn-surfline.com" }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +26,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
+// Add headers
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
